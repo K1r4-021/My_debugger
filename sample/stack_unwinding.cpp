23935275e2d86edb69a9d84674e763cba6d50fea
Arjav Jain <154581572+K1r4-021@users.noreply.github.com>
2024-07-24 20:51:06 +0530

    Add files via upload

---
+++ sample/stack_unwinding.cpp
@@ -0,0 +1,50 @@
+// void a() {
+//     int foo = 1;
+// }
+
+// void b() {
+//     int foo = 2;
+//     a();
+// }
+
+// void c() {
+//     int foo = 3;
+//     b();
+// }
+
+// void d() {
+//     int foo = 4;
+//     c();
+// }
+
+// void e() {
+//     int foo = 5;
+//     d();
+// }
+
+// void f() {
+//     int foo = 6;
+//     e();
+// }
+
+// int main() {
+//     f();
+// }
+
+
+void a() {
+    //stopped here
+}
+
+void b() {
+     a();
+}
+
+void c() {
+     a();
+}
+
+int main() {
+    b();
+    c();
+}

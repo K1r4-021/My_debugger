235afc4c2c4584d5954d1c52866be2a16dd1a3b4
Arjav Jain <154581572+K1r4-021@users.noreply.github.com>
2024-07-24 20:48:22 +0530

    Update debugger.hpp

--- include/debugger.hpp
+++ include/debugger.hpp
@@ -22,6 +22,59 @@ std::vector<std::string> split(const std::string &s, char delimiter)
     return out;
 }
 
+enum class symbol_type
+{
+    notype,  // No type (e.g., absolute symbol)
+    object,  // Data object
+    func,    // Function entry point
+    section, // Symbol is associated with a section
+    file,    // Source file associated with the
+}; // object file
+
+std::string to_string(symbol_type st)
+{
+    switch (st)
+    {
+    case symbol_type::notype:
+        return "notype";
+    case symbol_type::object:
+        return "object";
+    case symbol_type::func:
+        return "func";
+    case symbol_type::section:
+        return "section";
+    case symbol_type::file:
+        return "file";
+    }
+    return NULL;
+}
+
+struct symbol
+{
+    symbol_type type;
+    std::string name;
+    std::uintptr_t addr;
+};
+
+symbol_type to_symbol_type(elf::stt sym)
+{
+    switch (sym)
+    {
+    case elf::stt::notype:
+        return symbol_type::notype;
+    case elf::stt::object:
+        return symbol_type::object;
+    case elf::stt::func:
+        return symbol_type::func;
+    case elf::stt::section:
+        return symbol_type::section;
+    case elf::stt::file:
+        return symbol_type::file;
+    default:
+        return symbol_type::notype;
+    }
+};
+
 class debugger
 {
 private:
@@ -35,11 +88,13 @@ private:
     void execute_continue();
     void handle_sigtrap(siginfo_t);
     void execute_exit();
+    void single_step_instruction();
+    void single_step_instruction_over_breakpoint_check();
     void load_address();
     siginfo_t get_signal_info();
     void wait_for_signal();
     bool has_debug_info(const elf::elf &);
-    std::map<std::intptr_t, breakpoint> breakpoints;
+    std::unordered_map<std::intptr_t, breakpoint> breakpoints;
 
 public:
     debugger(std::string prog_n, pid_t p) : prog_name{std::move(prog_n)}, pid(p)
@@ -53,16 +108,55 @@ public:
                       << std::endl;
     }
     void run();
+    void read_variable();
+    void backtrace();
     void set_breakpoint_at(std::intptr_t);
+    void set_breakpoint_at_address(std::intptr_t);
+    void set_breakpoint_at_function(const std::string &);
+    void set_breakpoint_at_source_line(const std::string &, unsigned);
     void dump_registers();
     void print_source(const std::string &, unsigned line, unsigned n_line_context = 2);
     void step_over_breakpoint();
     void info_breakpoints();
-    void delete_breakpoints();
+    void step_out();
+    void step_in();
+    void step_over();
+    std::vector<symbol> lookup_symbol(const std::string &);
+    void delete_breakpoint(std::intptr_t);
     uint64_t offset_load_address(uint64_t addr) { return addr - load_addr; }
+    uint64_t offset_dwarf_address(uint64_t addr) { return addr + load_addr; }
     auto get_function_from_pc(uint64_t pc) -> dwarf::die;
     auto get_line_entry_from_pc(uint64_t pc) -> dwarf::line_table::iterator;
+    auto read_memory(uint64_t address) -> uint64_t;
+    void write_memory(uint64_t address, uint64_t value);
 };
+
+class ptrace_expr_context : public dwarf::expr_context
+{
+private:
+    pid_t pid;
+    uint64_t load_address;
+
+public:
+    ptrace_expr_context(pid_t m_pid,uint64_t m_load_address) : pid(m_pid),load_address(m_load_address) {}
+
+    dwarf::taddr reg(unsigned regnum) override
+    {
+        return get_register_value_from_dwarf_regiaters(pid, regnum);
+    }
+    // dwarf::taddr pc() override{
+    //     struct user_regs_struct regs;
+    //     ptrace(PTRACE_GETREGS,pid,nullptr,&regs);
+    //     return regs.rip-load_address;
+    // }
+    dwarf::taddr deref_size(dwarf::taddr address, unsigned size) override
+    {
+        // TODO take into account size
+        return ptrace(PTRACE_PEEKDATA, pid, address, nullptr);
+    }
+};
+
+template class std::initializer_list<dwarf::taddr>;
 
 void debugger::run()
 {
@@ -76,6 +170,144 @@ void debugger::run()
         handle_command(line);
         linenoiseHistoryAdd(line);
         // linenoiseHistoryFree();
+    }
+}
+
+uint64_t debugger::read_memory(uint64_t address)
+{
+    return ptrace(PTRACE_PEEKDATA, pid, address, nullptr);
+}
+
+void debugger::write_memory(uint64_t address, uint64_t value)
+{
+    ptrace(PTRACE_POKEDATA, pid, address, value);
+}
+
+void debugger::backtrace()
+{
+    auto output_frame = [frame_number = 0](auto &&func) mutable
+    {
+        std::cout << "frame #" <<frame_number++ << ":\x1b[38;5;33m 0x" << dwarf::at_low_pc(func) << " \x1b[0m" << dwarf::at_name(func) << std::endl;
+    };
+    auto current_func = get_function_from_pc(offset_load_address(get_rip(pid)));
+    output_frame(current_func);
+    auto frame_pointer = get_register_value(pid, reg::rbp);
+    auto return_address = read_memory(frame_pointer + 8);
+    while (dwarf::at_name(current_func) != "main")
+    {
+        current_func = get_function_from_pc(offset_load_address(return_address));
+        output_frame(current_func);
+        frame_pointer = read_memory(frame_pointer);
+        return_address = read_memory(frame_pointer + 8);
+    }
+}
+
+void debugger::single_step_instruction()
+{
+    ptrace(PTRACE_SINGLESTEP, pid, nullptr, nullptr);
+    wait_for_signal();
+}
+
+void debugger::single_step_instruction_over_breakpoint_check()
+{
+    if (breakpoints.count(get_rip(pid)))
+        step_over_breakpoint();
+    else
+        single_step_instruction();
+}
+
+void debugger::step_out()
+{
+    auto return_address = read_memory(get_register_value(pid, reg::rbp) + 8);
+    bool should_remove_breakpoint = false;
+    if (!breakpoints.count(return_address))
+    {
+        set_breakpoint_at_address(return_address);
+        should_remove_breakpoint = true;
+    }
+    execute_continue();
+    if (should_remove_breakpoint)
+        delete_breakpoint(return_address);
+}
+
+void debugger::step_in()
+{
+    auto line = get_line_entry_from_pc(offset_load_address(get_rip(pid)))->line;
+
+    while (get_line_entry_from_pc(offset_load_address(get_rip(pid)))->line == line)
+    {
+        single_step_instruction_over_breakpoint_check();
+    }
+
+    auto line_entry = get_line_entry_from_pc(offset_load_address(get_rip(pid)));
+    print_source(line_entry->file->path, line_entry->line);
+}
+
+void debugger::step_over()
+{
+    auto func = get_function_from_pc(offset_load_address(get_rip(pid)));
+    auto func_entry = at_low_pc(func);
+    auto func_end = at_high_pc(func);
+    auto line = get_line_entry_from_pc(func_entry);
+    auto start_line = get_line_entry_from_pc(offset_load_address(get_rip(pid)));
+
+    std::vector<std::intptr_t> to_delete{};
+
+    while (line->address < func_end)
+    {
+        auto load_address = offset_dwarf_address(line->address);
+        if (line->address != start_line->address && !breakpoints.count(load_address))
+        {
+            set_breakpoint_at_address(load_address);
+            to_delete.push_back(load_address);
+        }
+        ++line;
+    }
+    auto return_address = read_memory(get_register_value(pid, reg::rbp) + 8);
+    if (!breakpoints.count(return_address))
+    {
+        set_breakpoint_at_address(return_address);
+        to_delete.push_back(return_address);
+    }
+
+    execute_continue();
+
+    for (auto addr : to_delete)
+    {
+        delete_breakpoint(addr);
+    }
+}
+
+void debugger::read_variable()
+{
+    auto func = get_function_from_pc(offset_load_address(get_rip(pid)));
+    for (const auto &die : func)
+    {
+        if (die.tag == dwarf::DW_TAG::variable)
+        {
+            auto val_loc = die[dwarf::DW_AT::location];
+            if (val_loc.get_type() == dwarf::value::type::exprloc)
+            {
+                ptrace_expr_context context(pid,load_addr);
+                auto result = val_loc.as_exprloc().evaluate(&context);
+                switch (result.location_type)
+                {
+                case dwarf::expr_result::type::address:
+                {
+                    auto value = read_memory(result.value);
+                    std::cout << dwarf::at_name(die) << " (0x" << std::hex << result.value << ") = " << value << std::endl;
+                }
+                case dwarf::expr_result::type::reg:
+                {
+                    auto value = get_register_value_from_dwarf_regiaters(pid, result.value);
+                    std::cout << dwarf::at_name(die) << " (ref " << result.value << ") = " << value << std::endl;
+                    break;
+                }
+                default:
+                    throw std::runtime_error("Unhandled variable location");
+                }
+            }
+        }
     }
 }
 
@@ -102,17 +334,18 @@ void debugger::wait_for_signal()
     int wait_status;
     auto options = 0;
     waitpid(pid, &wait_status, options);
-    //activate for dwarf -4 only
-    auto siginfo =get_signal_info();
-    switch(siginfo.si_signo){
-        case SIGTRAP:
+    // activate for dwarf -4 only
+    auto siginfo = get_signal_info();
+    switch (siginfo.si_signo)
+    {
+    case SIGTRAP:
         handle_sigtrap(siginfo);
         break;
-        case SIGSEGV:
-        std::cout<<"OOPS :( segfault. Reason: "<<siginfo.si_code<<std::endl;
+    case SIGSEGV:
+        std::cout << "OOPS :( segfault. Reason: " << siginfo.si_code << std::endl;
         break;
-        default:
-        std::cout<<strsignal(siginfo.si_code)<<std::endl;
+    default:
+        std::cout << strsignal(siginfo.si_code) << std::endl;
     }
 }
 
@@ -126,12 +359,14 @@ void debugger::info_breakpoints()
     }
 }
 
-// void debugger::delete_breakpoints(){
-//     for(auto &b_p:breakpoints){
-//         breakpoints.erase(b_p.first);
-//     }
-//     step_over_breakpoint();
-// }
+void debugger::delete_breakpoint(std::intptr_t bp_addr)
+{
+    if (breakpoints.at(bp_addr).is_b_enable())
+    {
+        breakpoints.at(bp_addr).b_disable();
+    }
+    breakpoints.erase(bp_addr);
+}
 
 void debugger::set_breakpoint_at(std::intptr_t address)
 {
@@ -146,8 +381,15 @@ void debugger::set_breakpoint_at(std::intptr_t address)
         std::cerr << "\x1b[38;5;160mCannot access memory at\x1b[0m: 0x" << std::hex << address << std::endl;
 }
 
+void debugger::set_breakpoint_at_address(std::intptr_t address)
+{
+    breakpoint bp(pid, address);
+    if (bp.b_enable())
+        breakpoints[address] = bp;
+}
+
 void debugger::dump_registers()
-{   
+{
     for (const auto &rd : dump_register_descriptors)
     {
         std::cout << "\x1b[38;5;196m" << std::setw(9) << std::left << rd.name << "\x1b[38;5;123m: 0x" << std::setfill('0') << std::setw(16) << std::right << std::hex << get_register_value(pid, rd.r) << "\x1b[0m" << std::setfill(' ') << std::endl;
@@ -156,14 +398,12 @@ void debugger::dump_registers()
 
 void debugger::step_over_breakpoint()
 {
-    auto breakpoint_loc = get_rip(pid) - 1;
-    if (breakpoints.count(breakpoint_loc))
+    if (breakpoints.count(get_rip(pid)))
     {
-        std::cout<<get_rip(pid)<<std::endl;
-        auto &bp = breakpoints[breakpoint_loc];
+        auto &bp = breakpoints[get_rip(pid)];
         if (bp.is_b_enable())
         {
-            //set_rip(pid, breakpoint_loc);   //for non dwarf activate it
+            // set_rip(pid, get_rip(pid)-1);   //for non dwarf activate it
             bp.b_disable();
             ptrace(PTRACE_SINGLESTEP, pid, nullptr, nullptr);
             wait_for_signal();
@@ -202,7 +442,8 @@ void debugger::handle_sigtrap(siginfo_t info)
         set_rip(pid, get_rip(pid) - 1);
         std::cout << "Breakpoint hit at: 0x" << std::hex << get_rip(pid) << std::endl;
         auto offset_pc = offset_load_address(get_rip(pid));
-        // std::cout<<get_rip(pid)<<" "<<offset_pc<<std::endl;
+        if (get_rip(pid) < load_addr)
+            uint64_t offset_pc = get_rip(pid);
         auto line_entry = get_line_entry_from_pc(offset_pc);
         print_source(line_entry->file->path, line_entry->line);
         return;
@@ -260,6 +501,71 @@ dwarf::line_table::iterator debugger::get_line_entry_from_pc(uint64_t pc)
 
     std::cerr << "PC: " << std::hex << pc << std::dec << " not in range: " << std::endl;
     throw std::out_of_range{"Cannot find line entry"};
+}
+
+void debugger::set_breakpoint_at_function(const std::string &name)
+{
+    for (const auto &cu : m_dwarf.compilation_units())
+    {
+        for (const auto &die : cu.root())
+        {
+            if (die.has(dwarf::DW_AT::name) && at_name(die) == name)
+            {
+                auto low_pc = at_low_pc(die);
+                auto entry = get_line_entry_from_pc(low_pc);
+                ++entry; // skip prologue
+                set_breakpoint_at(offset_dwarf_address(entry->address));
+            }
+            else if (!die.has(dwarf::DW_AT::name))
+                std::cerr << "\x1b[31mInvalid argument\x1b[0m" << std::endl;
+        }
+    }
+}
+
+void debugger::set_breakpoint_at_source_line(const std::string &file, unsigned line)
+{
+    for (const auto &cu : m_dwarf.compilation_units())
+    {
+        if (file.find(at_name(cu.root())) == 0)
+        {
+            const auto &lt = cu.get_line_table();
+
+            for (const auto &entry : lt)
+            {
+                if (entry.is_stmt && entry.line == line)
+                {
+                    set_breakpoint_at(offset_dwarf_address(entry.address));
+                    return;
+                }
+            }
+        }
+    }
+}
+
+std::vector<symbol> debugger::lookup_symbol(const std::string &name)
+{
+    std::vector<symbol> syms;
+    for (auto &sec : m_elf.sections())
+    {
+        if (sec.get_hdr().type != elf::sht::symtab && sec.get_hdr().type != elf::sht::dynsym)
+            continue;
+
+        for (auto sym : sec.as_symtab())
+        {
+            if (sym.get_name() == name)
+            {
+                auto &d = sym.get_data();
+                syms.push_back(symbol{to_symbol_type(d.type()), sym.get_name(), d.value});
+            }
+            else if (name == "all")
+            {
+                auto &d = sym.get_data();
+                syms.push_back(symbol{to_symbol_type(d.type()), sym.get_name(), d.value});
+            }
+        }
+    }
+
+    return syms;
 }
 
 void debugger::print_source(const std::string &file_name, unsigned line, unsigned n_lines_context)
